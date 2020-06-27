dcb7d72547a1f37652be2e31f120c2bf98e3fa58
fenixlinuxos <66138813+fenixlinuxos@users.noreply.github.com>
2020-06-27 18:28:40 +0200

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -5,16 +5,16 @@ while :
 do
 echo " -------------------------------------------------------------"
 echo "1. Comprobar actualizaciones "
-echo "2. Actualizar"
+echo "2. Actualizar (por el momento no hay parches), se hará un apt upgrade."
 echo "3. Novedades fenix os"
 echo "q. Salir"
 echo -n "Seleccione una opción [1 - 3] "
 echo " -------------------------------"
 read opcion
 case $opcion in
-1) echo "No hay actualizaciones";;
+1) echo "FenixOS 0.7 Ya está desponible.";;
 2) sudo apt update & sudo apt dist-upgrade & sudo apt clean;;
-3) echo "No hay novedades";;
+3) echo "Se ha lanzado FenixOS 0.7";;
 q) echo -e "Gracias por usar el asistente, chao.\n
 Nota: Se irán añadiendo más funciones en cada versión.\n\n2020androrama ";
  sleep 3; exit 1;;
