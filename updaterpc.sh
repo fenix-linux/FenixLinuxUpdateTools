91e7ad95cfdabe4bd7f46984be4274cd9db75783
fenixlinuxos <66138813+fenixlinuxos@users.noreply.github.com>
2020-12-10 12:07:30 +0100

    the option to update the system is again available

--- updaterpc.sh
+++ updaterpc.sh
@@ -6,26 +6,23 @@ do
 echo " -------------------------------------------------------------"
 echo "1. Check for updates "
 echo "2. Fenix os novelties"
-
+echo "3. Dist upgrade"
 echo "q. Exit"
 echo -n "Select an option [1 - 2] "
-echo "1. Comprobar actualizaciones "
-echo "2. Novedades fenix os"
-echo "q. Salir"
-echo -n "Seleccione una opción [1 - 2] "
 echo " -------------------------------"
 read opcion
 case $opcion in
-1) echo "FenixPI 0.7 Ya está desponible.";;
-2) echo "Se ha lanzado FenixPI 0.7";;
+1) echo "Fenix Release Candidte based on Ubuntu available Soon";;
+2) echo "Fenix Release Candidte based on Ubuntu available Soon";;
+3)sudo apt update --fix-missing && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt install -f && sudo dpkg --configure -a
 
-q) echo -e "Gracias por usar el asistente, chao.\n
-Nota: Se irán añadiendo más funciones en cada versión.\n\n2020androrama ";
+q) echo -e "chao.\n
+fenixlinux.com ";
 echo -e "Thanks for using the assistant, bye.
-Note: More functions will be added in each version. 2020androrama ";
+Note: More functions will be added in each version. fenixlinux.com ";
  sleep 3; exit 1;;
-*) echo "$opc es una opción inválida/ is an invalid option. ";
-echo "Presiona una tecla para continuar/Press a key to continue/...";
+*) echo "$opc is an invalid option. ";
+echo "Press a key to continue/...";
 read foo;;
 esac
 done
