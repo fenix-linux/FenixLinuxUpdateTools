ee1b8d25348625c7c629308cf77d275a94a2f490
fenixlinuxos <66138813+fenixlinuxos@users.noreply.github.com>
2020-12-10 12:08:06 +0100

    Delete parche1.sh

--- parche1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cat /dev/null > /boot/cmdline.txt
-echo "console=serial0,115200 console=tty1 root=PARTUUID=ea7d04d6-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait logo.nologo" > /boot/cmdline.txt
