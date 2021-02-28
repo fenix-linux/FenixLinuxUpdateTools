58ab7c44a561c2cb03c6eb506f52392567ea92bd
fenixlinuxos <66138813+fenixlinuxos@users.noreply.github.com>
2021-03-01 00:48:50 +0100

    copied code from fenix updater pc

--- FenixPi-Updater.sh
+++ FenixPi-Updater.sh
@@ -5,19 +5,24 @@ while :
 do
 echo " -------------------------------------------------------------"
 echo "1. Check for updates "
-echo "2. Novelties"
+echo "2. Fenix os novelties"
+echo "3. Dist upgrade"
 echo "q. Exit"
-echo -n "Select an option [1 - 4] "
+echo -n "Select an option [1 - 3] "
 echo " -------------------------------"
 read opcion
 case $opcion in
-1) echo "New release soon 10-dic-2020";;
-2) echo "New release soon 10-dic-2020";;
+1) echo "Test";;
+2) echo "Test";;
+3) sudo apt update --fix-missing && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt install -f && sudo dpkg --configure -a;;
 
-q) echo -e "chao.\n";
+q) echo -e "chao.\n
+   fenixlinux.com ";
+echo -e "Thanks for using the assistant, bye.
+Note: More functions will be added in each version. fenixlinux.com ";
  sleep 3; exit 1;;
-*) echo "$opc invalid option ";
-echo "Press a key to continue...";
+*) echo "$opc is an invalid option. ";
+echo "Press a key to continue/...";
 read foo;;
 esac
 done
